42b296a91eaa6f96143a6158def714138ea3507d
Florin <florinbizgan2000@gmail.com>
2022-10-23 15:45:28 +0100

    content, contact and skill .json files added

--- src/component/my-cv/my-cv.jsx
+++ src/component/my-cv/my-cv.jsx
@@ -3,8 +3,10 @@ import { Button } from '..';
 const MyCV = () => {
   return (
     <div>
+      {/* This is a temporary solution until a proper icon display will be used */}
       <Button pad="nimic" textcol="white" color="funGreen">Download</Button>
       <Button pad="nimic" textcol="white" color="milanoRed">Subscribe</Button>
+      <p>Icons are at the momment a temp solution</p>
 
     </div>
   )
